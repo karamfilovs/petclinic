0e87cac7158c10bcedbb10cd7573a6eda1b1ee43
Aleksandar Karamiflov <aleksandar.karamfilov.extern@joyn.de>
2021-08-19 21:23:06 +0300

    Installing Firefox latest

--- src/test/java/core/BaseTest.java
+++ src/test/java/core/BaseTest.java
@@ -22,7 +22,7 @@ public class BaseTest {
 
     @BeforeAll
     public static void beforeAll(){
-        WebDriverManager.chromedriver().setup();
+        WebDriverManager.firefoxdriver().setup();
     }
 
     @BeforeEach
